74cfa4ab91a13bf0494e8f1cd476163ee4a2ea80
Ebenezer Ackon <Ebenezer@younow.com>
2018-07-27 01:52:54 -0400

    add greeter app interaction (closes #2, closes #3)

--- app/src/main/java/iwd/props/com/greeterapp/MainActivity.kt
+++ app/src/main/java/iwd/props/com/greeterapp/MainActivity.kt
@@ -3,10 +3,15 @@ package iwd.props.com.greeterapp
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import org.web3j.crypto.Credentials
+import org.web3j.protocol.Web3jFactory
+import org.web3j.protocol.infura.InfuraHttpService
+import java.math.BigInteger
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,9 +20,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        val contractAddress = "0x8394cDf176A4A52DA5889f7a99c4f7AD2BF59088"
+        val url = "https://rinkeby.infura.io/v3/01eb8f7b5e514832af8e827c23784d23"
+        val web3j = Web3jFactory.build(InfuraHttpService(url))
+        val gasLimit: BigInteger = BigInteger.valueOf(21000)
+        val gasPrice: BigInteger = BigInteger.valueOf(600000)
+
+        val credentials: Credentials = Credentials.create(
+                "f9319fe162c31947c0ca8fd649a536b7ca311b5f210afdc48b62fd7d18ce53e4","0x2Dd361B86bb18FDe3cdfDD11b9aC7461C0FB2Ea2")
+
+        val greeter = Greeter(contractAddress, web3j, credentials ,gasLimit, gasPrice)
+
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+
+            val thread = Thread(Runnable {
+                try {
+                    /*** SIMPLE EXECUTION ONLY FOR DEMO PURPOSES.... THIS THREAD INITIALIZATION HERE IS BAD PRACTICE ***/
+                    //Log.d("Ebenezer", " ${greeter.isValid}")
+                    greeter.changeGreeting("Greeting changed from an Android App (ಠ_ಠ) ")
+                    greeter.greet()
+                    //greeter.kill()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            })
+
+            thread.start()
+
+
         }
     }
 
